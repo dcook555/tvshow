39d99574bf202eac0ef99e8f6ca068d393395163
Devin Cook <programmer@hs-lab.local>
2015-12-23 10:37:55 -0500

    working

--- tvshow.py
+++ tvshow.py
@@ -1,5 +1,13 @@
 import random
+def viewer():
     viewers=random.randint(0,320090000)
     leavers=random.randint(0,160045000)
-    print viewers-leavers
-    
+def total():
+    totalviewers=viewers-leavers
+def final_tally():
+    if totalviewers==0:
+        print ("off-air")
+viewer()
+total()
+final_tally()
+print viewers-leavers
