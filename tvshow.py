2dc40004a4d9ab0d55db357e82721f0078486290
Devin Cook <programmer@hs-lab.local>
2016-01-08 09:16:16 -0500

    new

--- tvshow.py
+++ tvshow.py
@@ -1,18 +1,25 @@
 import random
 class show:
-    
-    def views():
-        Viewers=random.randint(0,320090000)
+    def viewers():
+        Views=random.randint(0,320090000)
         season=random.randint(0,15)
         if season==(0,3):
             leavers=random.randint(0,160045000)
-    def total(views):
-        totalviewers=Viewers-leavers
+    def recomendation():
+        recomend=random.randint(0,15)
+        if recomend==(0,5):
+            pass
+        elif recomend==(6.15):
+            recomended=random.randint(0,2000)
+            views+recomended==totalviewers
+    def total():
+        totals=totalviewers-leavers
         if totalviewers<0:
             print ("off-air")          
     def results():
         print totalviewers
     
-views()
-total(views)
+viewers()
+recomendation()
+total()
 results()
